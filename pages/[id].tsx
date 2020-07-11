750aae0ea44f63c94fcbf37e5900c16143da5a68
lrosa007 <lucas@10factory.com>
2020-07-10 20:25:14 -0400

    feat: get a single plant by id

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,5 +1,40 @@
-const SinglePlantPage = () => {
-  return <></>;
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+
+import { getToken } from "~/utils/token";
+
+import { getPlant, Plant } from "~/trefle";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.params.id as any;
+
+  const token = await getToken(context);
+  const plant = await getPlant(id, token);
+
+  return {
+    props: {
+      plant,
+    },
+  };
+};
+
+interface SinglePlantPageProps {
+  plant: Plant;
+}
+
+const SinglePlantPage = ({ plant }: SinglePlantPageProps) => {
+  return (
+    <div className="container">
+      <Head>
+        <title>Plant Wiki</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">{plant.common_name}</h1>
+      </main>
+    </div>
+  );
 };
 
 export default SinglePlantPage;
