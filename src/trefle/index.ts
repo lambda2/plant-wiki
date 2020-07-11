23f5a732cd9ad88faeb646af3c17ef5cd371009d
lrosa007 <lucas@10factory.com>
2020-07-10 22:27:56 -0400

    chore: pass params more cleanly to axios

--- src/trefle/index.ts
+++ src/trefle/index.ts
@@ -15,65 +15,65 @@ import {
 export * from "./types";
 
 export const getAllKingdoms = async (token: string) => {
-  const { data } = await axios.get<AllKingdoms>(
-    `${baseURL}/kingdoms?token=${token}`
-  );
+  const { data } = await axios.get<AllKingdoms>(`${baseURL}/kingdoms`, {
+    params: { token },
+  });
 
   return data;
 };
 
 export const getKingdom = async (id: number, token: string) => {
-  const { data } = await axios.get<Kingdom>(
-    `${baseURL}/kingdoms/${id}?token=${token}`
-  );
+  const { data } = await axios.get<Kingdom>(`${baseURL}/kingdoms/${id}`, {
+    params: { token },
+  });
 
   return data;
 };
 
 export const getAllSubkingdoms = async (token: string) => {
-  const { data } = await axios.get<AllSubkingdoms>(
-    `${baseURL}/subkingdoms?token=${token}`
-  );
+  const { data } = await axios.get<AllSubkingdoms>(`${baseURL}/subkingdoms`, {
+    params: { token },
+  });
 
   return data;
 };
 
 export const getSubkingdom = async (id: number, token: string) => {
-  const { data } = await axios.get<Subkingdom>(
-    `${baseURL}/subkingdoms/${id}?token=${token}`
-  );
+  const { data } = await axios.get<Subkingdom>(`${baseURL}/subkingdoms/${id}`, {
+    params: { token },
+  });
 
   return data;
 };
 
 export const getAllPlants = async (token: string) => {
-  const { data } = await axios.get<AllPlants>(
-    `${baseURL}/plants?token=${token}`
-  );
+  const { data } = await axios.get<AllPlants>(`${baseURL}/plants`, {
+    params: { token },
+  });
 
   return data;
 };
 
 export const getPlant = async (id: number, token: string) => {
-  const { data } = await axios.get<Plant>(
-    `${baseURL}/plants/${id}?token=${token}`
-  );
+  const { data } = await axios.get<Plant>(`${baseURL}/plants/${id}`, {
+    params: { token },
+  });
 
   return data;
 };
 
 export const getAllSpecies = async (token: string) => {
-  const { data } = await axios.get<AllSpecies>(
-    `${baseURL}/species?token=${token}`
-  );
+  const { data } = await axios.get<AllSpecies>(`${baseURL}/species`, {
+    params: { token },
+  });
 
   return data;
 };
 
 export const getSpecies = async (id: number, token: string) => {
-  const { data } = await axios.get<Species>(
-    `${baseURL}/species/${id}?token=${token}`
-  );
+  const { data } = await axios.get<Species>(`${baseURL}/species/${id}`, {
+    params: { token },
+  });
 
   return data;
 };
