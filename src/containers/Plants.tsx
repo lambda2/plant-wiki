8f6824b6a50df372294b8306a08850790693a6da
joseph-engelmajer <joseph.engelmajer@gmail.com>
2020-07-12 16:52:31 -0400

    feat(styles): plant card hover

--- src/containers/Plants.tsx
+++ src/containers/Plants.tsx
@@ -41,6 +41,13 @@ const Data = ({ plants }: { plants: AllPlants }) => {
               borderRadius: 4,
               boxShadow:
                 "0 4px 6px 1px #9B9B9B20, 0 1px 7px 1px #9B9B9B20, 0 2px 2px -1px #9B9B9B20",
+              cursor: "pointer",
+              transition: "all 0.3s ease-in-out",
+              ":hover": {
+                boxShadow:
+                  "0 6px 8px 3px #A5C57725, 0 2px 12px 2px #A5C57725, 0 4px 4px -2px #A5C57725",
+                borderColor: "#A5C57785",
+              },
             })}
           >
             <p>
