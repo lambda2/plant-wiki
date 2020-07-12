68251b0b06a20f21ef1b6788766c4dcda525ef5e
joseph-engelmajer <joseph.engelmajer@gmail.com>
2020-07-12 16:57:41 -0400

    feat(styles): plant card border opacity

--- src/containers/Plants.tsx
+++ src/containers/Plants.tsx
@@ -37,7 +37,7 @@ const Data = ({ plants }: { plants: AllPlants }) => {
           <div
             className={css({
               padding: "8px 16px",
-              border: "1px solid #9B9B9B75",
+              border: "1px solid #9B9B9B10",
               borderRadius: 4,
               boxShadow:
                 "0 4px 6px 1px #9B9B9B20, 0 1px 7px 1px #9B9B9B20, 0 2px 2px -1px #9B9B9B20",
@@ -46,7 +46,7 @@ const Data = ({ plants }: { plants: AllPlants }) => {
               ":hover": {
                 boxShadow:
                   "0 6px 8px 3px #A5C57725, 0 2px 12px 2px #A5C57725, 0 4px 4px -2px #A5C57725",
-                borderColor: "#A5C57785",
+                borderColor: "#A5C57710",
               },
             })}
           >
