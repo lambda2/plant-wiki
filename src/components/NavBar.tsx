4a0f439e609146186ee7a591236e0186ec46746a
Isaac Weinbach <isaacweinbach@gmail.com>
2020-07-12 22:47:10 -0400

    feat(NavBar):adds github repo icon to

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,15 @@
 import { css } from "otion";
 import Link from "next/link";
+import { authEndpoint } from "~/constants";
 
 const NavBar = () => {
   return (
-    <nav className={css({ paddingLeft: 16, paddingRight: 16 })}>
+    <nav
+      className={css({
+        paddingLeft: 16,
+        paddingRight: 16,
+      })}
+    >
       <ul
         className={css({
           display: "flex",
@@ -18,13 +24,28 @@ const NavBar = () => {
             <div
               className={css({
                 backgroundImage: "url(/logo.svg)",
-                height: "20px",
-                width: "50px",
+                height: "30px",
+                width: "45px",
                 backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
                 flex: "1",
               })}
             ></div>
           </Link>
+        </li>
+        <li>
+          <a href="https://github.com/10factory/plant-wiki" target="_blank">
+            <div
+              className={css({
+                backgroundImage: "url(/github-branch.svg)",
+                height: "30px",
+                width: "30px",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+                flex: "1",
+              })}
+            ></div>
+          </a>
         </li>
       </ul>
     </nav>
